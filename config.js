7d4ef2112040d8556cf971787b59aa758c35dcec
pabitrabiswal023-glitch <pabitrabiswal023@gmail.com>
2026-09-15 08:15:07 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  BOT_NAME: "KRISHU-MD",
+  OWNER_NUMBER: "919999999999",   // your number, country code, no +
+  PREFIX: ".",
+  PAIRING_MODE: true,             // true = pairing code (works on mobile!)
+  SESSION_DIR: "./session"
+};
